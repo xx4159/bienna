6abf2e8286f46550281bbd00f1f544c83c305447
eunmi jeon <emjeon93@29cm.co.kr>
2016-10-27 15:48:26 +0900

    modify - webpack configuration
    - babel-loader, react
    - style/css/sass loader

---
+++ src/bienna.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+require('./bienna.scss');
+
+ReactDom.render(
+  <div></div>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,18 @@ module.exports = {
     path: './bin',
     filename: 'bienna.bundle.js',
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style', 'css', 'sass'],
+      },
+    ],
+  },
 };
