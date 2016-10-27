3595af7104f9b3ab907995e105aa0080e911dab2
eunmi jeon <emjeon93@29cm.co.kr>
2016-10-27 19:27:47 +0900

    modify - define `Bienna` component function

--- src/bienna.js
+++ src/bienna.js
@@ -3,7 +3,23 @@ import ReactDom from 'react-dom';
 
 require('./bienna.scss');
 
+function Bienna(props) {
+  return <div id="bienna" className="bienna">
+    <div className="bienna__wrap">
+      <div className="bienna__inner">
+        <div className="bienna__header">{ props.title }</div>
+        <div className="bienna__body">{ props.text }</div>
+      </div>
+      <button type="button" id="biennaClose" className="bienna__button">OK</button>
+    </div>
+  </div>;
+}
+
+const element = (
+  <Bienna title="title" text="text" />
+);
+
 ReactDom.render(
-  <div></div>,
+  element,
   document.getElementById('root')
 );
