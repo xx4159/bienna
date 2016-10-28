aa5018e9c41e8ec007138e1e2d466f3d0a4dfac5
eunmi jeon <emjeon93@29cm.co.kr>
2016-10-28 19:32:11 +0900

    modify - componet as class

--- src/bienna.js
+++ src/bienna.js
@@ -3,16 +3,37 @@ import ReactDom from 'react-dom';
 
 require('./bienna.scss');
 
-function Bienna(props) {
-  return <div id="bienna" className="bienna">
-    <div className="bienna__wrap">
-      <div className="bienna__inner">
-        <div className="bienna__header">{ props.title }</div>
-        <div className="bienna__body">{ props.text }</div>
-      </div>
-      <button type="button" id="biennaClose" className="bienna__button">OK</button>
-    </div>
-  </div>;
+class Bienna extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+
+    this.open = this.open.bind(this);
+  }
+
+  open() {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div id="bienna" className="bienna">
+          <div className="bienna__wrap">
+            <div className="bienna__inner">
+              <div className="bienna__header">{ this.props.title }</div>
+              <div className="bienna__body">{ this.props.text }</div>
+            </div>
+            <button type="button"
+              id="biennaClose"
+              className="bienna__button"
+              onClick={ this.open }>OK</button>
+          </div>
+        </div>
+    );
+  }
 }
 
 const element = (
